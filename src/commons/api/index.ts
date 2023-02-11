f558f9456a8f24d974759664875f6d9c1f7a3e26
22sook00 <22sook00@gmail.com>
2023-02-11 16:30:37 +0900

    DEV-23 리액트쿼리 설정 및 CRUD 테스트 코드적용

--- src/commons/api/index.ts
+++ src/commons/api/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const apiInstance = axios.create({
+  //ex- process.env.NEXT_PUBLIC_BASE_URL
   baseURL: '',
 })
---
+++ src/commons/types/subTypes.ts
@@ -0,0 +1,4 @@
+export interface dataProps {
+  id: number
+  inputData: string
+}
