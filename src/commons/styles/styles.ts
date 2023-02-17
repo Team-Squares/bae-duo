da789154bb09644048441f26158fde55b7535053
22sook00 <22sook00@gmail.com>
2023-02-18 01:13:29 +0900

    DEV-41 Tag 공통컴포넌트 생성

--- src/commons/styles/styles.ts
+++ src/commons/styles/styles.ts
@@ -2,9 +2,10 @@ import { css } from '@emotion/react'
 
 export const color = {
   $default: '#828282',
-  $point: '#e91e63',
+  $point: '#4263EB',
   $sub: '#8ca315',
   $main: '#ffeb3b',
+  $warning: '#e91e63',
 }
 
 export const globalStyles = css`
