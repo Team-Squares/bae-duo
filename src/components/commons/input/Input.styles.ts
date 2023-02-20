0e718333fb39aa86ee79b06f1162c5aad5820f4b
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2023-02-20 22:45:29 +0900

    DEV-40 input 컴포넌트 초기 생성

--- src/components/commons/input/Input.styles.ts
+++ src/components/commons/input/Input.styles.ts
@@ -1,5 +1,17 @@
 import styled from '@emotion/styled'
 
-export const InputContainer = styled.div``
+export const InputContainer = styled.div<{ width: string }>`
+  width: ${props => props.width};
+`
 
-export const Input = styled.input``
+export const Label = styled.label`
+  font-size: 14px;
+  margin-bottom: 12px;
+  display: block;
+`
+
+export const Input = styled.input`
+  padding: 5px 10px;
+  border-radius: 10px;
+  width: 100%;
+`
--- src/components/commons/input/Input.tsx
+++ src/components/commons/input/Input.tsx
@@ -2,12 +2,18 @@ import React, { FC } from 'react'
 import * as Styled from './Input.styles'
 
 interface InputProps {
-  text: string
+  placeholder: string
+  type: string
+  label: string
+  width: string
 }
-const Input: FC<InputProps> = ({ text }) => {
+const Input: FC<InputProps> = ({ placeholder, type, label, width }) => {
+  const inputStyle = width || '100%'
+
   return (
-    <Styled.InputContainer>
-      <Styled.Input placeholder={text} />
+    <Styled.InputContainer width={inputStyle}>
+      <Styled.Label>{label}</Styled.Label>
+      <Styled.Input placeholder={placeholder} type={type} />
     </Styled.InputContainer>
   )
 }
