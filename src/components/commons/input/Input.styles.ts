329791c9811cce7a2fb4d92189356e3db3a12340
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2023-02-26 20:38:59 +0900

    DEV-40 input 옵션 추가

--- src/components/commons/input/Input.styles.ts
+++ src/components/commons/input/Input.styles.ts
@@ -1,45 +1,100 @@
 import styled from '@emotion/styled'
 import { css } from '@emotion/react'
+import { color } from '@/src/commons/styles/styles'
 
 const smInputStyles = css`
   width: 200px;
-  padding: 10px 15px;
+  input {
+    font-size: 14px;
+    padding: 15px;
+    &::placeholder {
+      font-size: 14px;
+    }
+  }
 `
 
 const mdInputStyles = css`
   width: 400px;
-  padding: 15px 18px;
+  input {
+    font-size: 16px;
+    padding: 18px 15px;
+    input::placeholder {
+      font-size: 14px;
+    }
+  }
 `
 
 const lgInputStyles = css`
-  width: 100%;
-  padding: 20px;
+  input {
+    padding: 20px 15px;
+    &::placeholder {
+      font-size: 14px;
+    }
+  }
+`
+
+const outlinedStyles = css`
+  border: 1px solid #dee2e6;
+  background-color: transparent;
+`
+
+const softStyles = css`
+  background-color: #f5f7fe;
+`
+
+const wrapperStyles = css`
+  padding: 15px;
+  border-radius: 10px;
+  input {
+    padding: 0;
+    border: none;
+    border-radius: 0;
+    &:focus {
+      outline: none;
+    }
+  }
+`
+
+const defaultStyles = css`
+  border: none;
+  background: transparent;
 `
 
 export const InputContainer = styled.div<{ size: string }>`
-  border-radius: 10px;
-  background-color: #f5f7fe;
+  margin: 30px; //삭제필요
   ${props => (props.size === 'sm' ? smInputStyles : props.size === 'md' ? mdInputStyles : lgInputStyles)}
 `
 
-export const Label = styled.label<{ label: string }>`
-  font-size: 12px;
-  display: block;
-  color: #868e96;
-  margin-bottom: ${props => (props.label ? '8px' : '0px')};
+export const LabelWrapper = styled.div<{ labelType: string; variant: string }>`
+  ${props => (props.variant === 'outlined' ? outlinedStyles : props.variant === 'soft' ? softStyles : '')};
+  ${props => (props.labelType === 'wrapper' ? wrapperStyles : defaultStyles)}
 `
 
-export const Input = styled.input`
+export const Label = styled.label<{ label: string }>`
+  font-size: 14px;
+  display: block;
+  color: #333;
+  margin-bottom: 5px;
+`
+
+export const Input = styled.input<{ variant: string }>`
   width: 100%;
   font-size: 16px;
   border: none;
-  background-color: #f5f7fe;
-
-  &::placeholder {
-    font-size: 14px;
-  }
-
+  border-radius: 10px;
+  ${props => (props.variant === 'outlined' ? outlinedStyles : props.variant === 'soft' ? softStyles : '')};
   &:focus {
     outline: none;
   }
+  &::placeholder {
+    color: #999;
+  }
+`
+
+export const HelperText = styled.p<{ helperTextColor: string }>`
+  color: ${props =>
+    props.helperTextColor === 'notice' ? `${color.$default}` : props.helperTextColor === 'error' ? 'red' : 'green'};
+  font-size: 12px;
+  margin-top: 10px;
+  padding-left: 5px;
 `
--- src/components/commons/input/Input.tsx
+++ src/components/commons/input/Input.tsx
@@ -4,18 +4,42 @@ interface InputProps {
   placeholder?: string
   type?: string
   label?: string
+  labelType?: 'wrapper' | 'default'
   size?: 'sm' | 'md' | 'lg'
+  variant?: 'plain' | 'outlined' | 'soft'
+  disabled?: boolean
+  helperText?: string
+  helperTextColor?: 'error' | 'success' | 'notice'
 }
 
-const Input: React.FC<InputProps> = ({ placeholder, type, label = '', size = 'md' }) => {
+const Input: React.FC<InputProps> = ({
+  placeholder,
+  type,
+  labelType = 'default',
+  label = '',
+  size = 'md',
+  variant = 'outlined',
+  disabled = false,
+  helperText,
+  helperTextColor = 'notice',
+}) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value)
   }
 
   return (
     <Styled.InputContainer size={size}>
-      <Styled.Label label={label}>{label}</Styled.Label>
-      <Styled.Input onChange={handleChange} placeholder={placeholder} type={type} />
+      <Styled.LabelWrapper labelType={labelType} variant={variant}>
+        <Styled.Label label={label}>{label}</Styled.Label>
+        <Styled.Input
+          onChange={handleChange}
+          placeholder={placeholder}
+          type={type}
+          disabled={disabled}
+          variant={variant}
+        />
+      </Styled.LabelWrapper>
+      <Styled.HelperText helperTextColor={helperTextColor}>{helperText}</Styled.HelperText>
     </Styled.InputContainer>
   )
 }
