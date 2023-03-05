31d644c2a87588878ef0c683dbe55616b6abcfff
keinn51 <soogood97@gmail.com>
2023-03-05 20:23:00 +0900

    DEV-49 펀딩 목록 카테고리와 연결

--- src/components/units/home/Home.tsx
+++ src/components/units/home/Home.tsx
@@ -51,49 +51,54 @@ const Home = () => {
             </button>
           ))}
         </Styled.CategoryBox>
-        {dummyData.map(item => (
-          <React.Fragment key={item.createdAt}>
-            <Styled.BrandsBox>
-              <Styled.BrandsCard>
-                <Styled.FundingInfo>
-                  <Styled.StatusBox>
-                    <Styled.FundingDate>{getCreateDate(item.createdAt)}</Styled.FundingDate>
-                    <Styled.Status>{categoryName[item.status]}</Styled.Status>
-                  </Styled.StatusBox>
-                  <div>
-                    <Styled.BrandName>{item.brand}</Styled.BrandName>
-                    <Styled.Starter>
-                      <Image src={tempProfileImg} alt="none"></Image>
-                      <span>{item.starter}</span>
-                      <Image src={starterImg} alt="none"></Image>
-                    </Styled.Starter>
-                  </div>
-                </Styled.FundingInfo>
-                <Styled.LimitBox>
-                  <div>
-                    <RiUser3Fill />
-                    <span>
-                      {item.curr_member}/{item.min_member} 참여
-                    </span>
-                  </div>
-                  <div>
-                    <FaClock />
-                    <span>{getEndTime(item.deadtime)} 마감</span>
-                  </div>
-                </Styled.LimitBox>
-                <Styled.ProgressBox>
-                  <Styled.Percentage>
-                    <span>{`${getPercentage(item.curr_price, item.total_price)}% 달성했어요`}</span>
-                    <span>{`${getKORMoneyString(item.curr_price)}원 / ${getKORMoneyString(item.total_price)}원`}</span>
-                  </Styled.Percentage>
-                  <Styled.ProgressBar percentage={getPercentage(item.curr_price, item.total_price)}>
-                    <div></div>
-                  </Styled.ProgressBar>
-                </Styled.ProgressBox>
-              </Styled.BrandsCard>
-            </Styled.BrandsBox>
-          </React.Fragment>
-        ))}
+        {dummyData.map(
+          item =>
+            (category === 0 || item.status === category) && (
+              <React.Fragment key={item.createdAt}>
+                <Styled.BrandsBox>
+                  <Styled.BrandsCard>
+                    <Styled.FundingInfo>
+                      <Styled.StatusBox>
+                        <Styled.FundingDate>{getCreateDate(item.createdAt)}</Styled.FundingDate>
+                        <Styled.Status>{categoryName[item.status]}</Styled.Status>
+                      </Styled.StatusBox>
+                      <div>
+                        <Styled.BrandName>{item.brand}</Styled.BrandName>
+                        <Styled.Starter>
+                          <Image src={tempProfileImg} alt="none"></Image>
+                          <span>{item.starter}</span>
+                          <Image src={starterImg} alt="none"></Image>
+                        </Styled.Starter>
+                      </div>
+                    </Styled.FundingInfo>
+                    <Styled.LimitBox>
+                      <div>
+                        <RiUser3Fill />
+                        <span>
+                          {item.curr_member}/{item.min_member} 참여
+                        </span>
+                      </div>
+                      <div>
+                        <FaClock />
+                        <span>{getEndTime(item.deadtime)} 마감</span>
+                      </div>
+                    </Styled.LimitBox>
+                    <Styled.ProgressBox>
+                      <Styled.Percentage>
+                        <span>{`${getPercentage(item.curr_price, item.total_price)}% 달성했어요`}</span>
+                        <span>{`${getKORMoneyString(item.curr_price)}원 / ${getKORMoneyString(
+                          item.total_price
+                        )}원`}</span>
+                      </Styled.Percentage>
+                      <Styled.ProgressBar percentage={getPercentage(item.curr_price, item.total_price)}>
+                        <div></div>
+                      </Styled.ProgressBar>
+                    </Styled.ProgressBox>
+                  </Styled.BrandsCard>
+                </Styled.BrandsBox>
+              </React.Fragment>
+            )
+        )}
       </div>
     </Styled.Container>
   )
