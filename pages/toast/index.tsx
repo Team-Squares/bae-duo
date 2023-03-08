6757bc8ac54f903defe4391733f6d8841b097677
jeongbeen2 <aceman9508@gmail.com>
2023-03-08 09:00:29 +0900

    DEV-36 토스트 구조변경 및 리코일 추가

--- pages/toast/index.tsx
+++ pages/toast/index.tsx
@@ -1,12 +1,17 @@
 import { useToast } from '@/src/commons/hooks/useToast'
 import * as Styled from '@/src/commons/hooks/useToast/useToast.styles'
+import { useRecoilState } from 'recoil'
+import { test } from '@/src/commons/atom/atom'
 
 const SubListPage = () => {
   const [pushToastQueue, ToastArea] = useToast({ delay: 5000 })
+  const [tester, setTester] = useRecoilState(test)
 
   return (
     <>
       <Styled.TestButtonArea>
+        <div>hi : {tester}</div>
+        <button onClick={() => setTester(tester + 1)}>counter!</button>
         <Styled.TestButton onClick={() => pushToastQueue('success', '성공했습니다.')}>Success</Styled.TestButton>
         <Styled.TestButton onClick={() => pushToastQueue('fail', '실패했습니다.')}>Fail</Styled.TestButton>
         <Styled.TestButton onClick={() => pushToastQueue('warning', '경고문구')}>Warning</Styled.TestButton>
