7b958a60be1e575e4c6f716422affca4e523f4a8
sseung95 <seung3657@gmail.com>
2023-03-14 20:20:33 +0900

    DEV-60 reset css 적용

--- src/commons/styles/styles.ts
+++ src/commons/styles/styles.ts
@@ -173,9 +173,53 @@ export const globalStyles = css`
   @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css');
 
   * {
-    margin: 0px;
-    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font: inherit;
     color: #212529;
-    font-family: 'Pretendard', sans-serif;
+    box-sizing: border-box;
+  }
+  *,
+  :after,
+  :before {
+    box-sizing: border-box;
+    flex-shrink: 0;
+  }
+  :root {
+    -webkit-tap-highlight-color: transparent;
+    -webkit-text-size-adjust: 100%;
+    text-size-adjust: 100%;
+    cursor: default;
+    line-height: 1.5;
+    overflow-wrap: break-word;
+    -moz-tab-size: 4;
+    tab-size: 4;
+  }
+  html {
+    font-family: 'Pretendard', 'Noto Sans', sans-serif;
+  }
+  html,
+  body {
+    height: 100%;
+  }
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+  button {
+    background: none;
+    border: 0;
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
   }
 `
