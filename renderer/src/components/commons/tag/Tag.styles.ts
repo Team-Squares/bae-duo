54433aa71630df15241742e4f95403af2fb02758
Lee Sookyoung <80618616+22sook00@users.noreply.github.com>
2023-04-22 17:23:52 +0900

    DEV-73 랜딩페이지 API 연결 및 빈데이터 화면 추가 (#28)
    * Fetch all fundinglist API on main
    
    * Fix all funding list style on main
    
    * Add empty section on main page due to no data
    
    * DEV-73 랜딩페이지 API 연결
    
    * Delete .env due to wrong post
    
    * test
    
    * DEV-73 랜딩페이지 API 연결
    
    * test
    
    * DEV-73 랜딩페이지 API 연결
    
    * DEV-73 카테고리분류에 따라 보여주기 및 스켈레톤 추가

--- renderer/src/components/commons/tag/Tag.styles.ts
+++ renderer/src/components/commons/tag/Tag.styles.ts
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled'
 
 export const TagContainer = styled.div<{ bgColor: string; color: string; size: string }>`
-  min-width: 75px;
+  min-width: 64px;
   width: fit-content;
-  padding: ${props => (props.size === 'sm' ? '6px 8px' : props.size === 'md' ? '8px 20px;' : '10px 24px')};
+  padding: ${props => (props.size === 'sm' ? '4px 8px' : props.size === 'md' ? '8px 20px;' : '10px 24px')};
   font-size: ${props => (props.size === 'sm' ? '10px' : props.size === 'md' ? '12px' : '14px')};
   font-weight: bold;
   text-align: center;
--- renderer/src/components/units/home/Home.tsx
+++ renderer/src/components/units/home/Home.tsx
@@ -3,22 +3,39 @@ import Image from 'next/image'
 import moment from 'moment'
 import * as Styled from './Home.styles'
 import { FaClock } from 'react-icons/fa'
+import { TbNotesOff } from 'react-icons/tb'
 import { RiUser3Fill, RiAddLine } from 'react-icons/ri'
 import { categoryName, getKORMoneyString, getPercentage, tagByStatus } from './data'
 import tempProfileImg from '@/public/images/profile_small.svg'
 import starterImg from '@/public/images/starter.svg'
-import dummyData from './dummy.json'
 import Button from '../../commons/button/Button'
 import Tag from '../../commons/tag/Tag'
+import { useQuery, useQueryClient } from 'react-query'
+import { getAllFundingList } from '@/src/commons/api/main/mainApi'
+import { getAllFundingListProps } from '@/src/commons/types/mainApi'
 import { useRouter } from 'next/router'
+import Skeleton from '../../commons/skeleton/Skeleton'
+import { Flex } from '../addFunding/AddFunding.styles'
 
 const Home = () => {
-  //const Moment = require('moment')
-  const [category, setCategory] = useState(0)
   const router = useRouter()
-  //useEffect(() => {
-  //  dummyData.sort((a, b) => new Moment(a.createdAt).format('YYYYMMDD') - new Moment(b.createdAt).format('YYYYMMDD'))
-  //}, [Moment])
+  const queryClient = useQueryClient() //delete할때 필요.
+  const [category, setCategory] = useState(0)
+  const [fundingList, setFundingList] = useState<getAllFundingListProps[]>([])
+  const { isLoading, isFetching, data, isSuccess } = useQuery(['getAllFundingList'], () => getAllFundingList())
+
+  useEffect(() => {
+    if (isSuccess) {
+      const sortByReverse = data.sort((a, b) => moment(b.createdAt).diff(a.createdAt))
+      const filterByCategory =
+        category === 1
+          ? sortByReverse.filter(list => list.status === 1)
+          : category === 2
+          ? sortByReverse.filter(list => list.status === 2)
+          : sortByReverse
+      setFundingList(filterByCategory)
+    }
+  }, [category, data, isSuccess])
 
   return (
     <div>
@@ -34,62 +51,77 @@ const Home = () => {
           펀딩 추가 <RiAddLine />
         </Button>
       </Styled.LandingHeader>
-      <Styled.BrandsBox>
-        {dummyData.map(
-          item =>
-            (category === 0 || item.status === category) && (
-              <Fragment key={item.createdAt}>
-                <Styled.BrandsCard>
-                  <Styled.FundingInfo>
-                    <Styled.StatusBox>
-                      {/*<Styled.FundingDate>{moment(item.createdAt).format('YYYY.MM.DD')}</Styled.FundingDate>*/}
-                      <Tag
-                        text={`펀딩 ${categoryName[item.status] ?? '실패'}`}
-                        color={tagByStatus(item.status)?.color}
-                        background={tagByStatus(item.status)?.background}
-                      />
-                    </Styled.StatusBox>
-                    <section>
-                      <Styled.BrandName>{item.brand}</Styled.BrandName>
-                      <Styled.Starter>
-                        <Image src={tempProfileImg} alt="none" width={16} height={16} />
-                        <span>{item.starter}</span>
-                        <Image src={starterImg} alt="none" />
-                      </Styled.Starter>
-                    </section>
-                  </Styled.FundingInfo>
-                  <Styled.LimitBox>
+      {isLoading ? (
+        <Flex gap={16}>
+          <Skeleton isCol />
+          <Skeleton isCol />
+          <Skeleton isCol />
+        </Flex>
+      ) : (
+        <Styled.BrandsBox>
+          {fundingList?.map(item => (
+            <Fragment key={item.createdAt}>
+              <Styled.BrandsCard>
+                <Styled.FundingInfo>
+                  <Styled.StatusBox>
+                    <Styled.FundingDate>{moment(item.createdAt).format('YYYY.MM.DD')}</Styled.FundingDate>
+                    <Tag
+                      text={`펀딩 ${categoryName[item.status] ?? '실패'}`}
+                      color={tagByStatus(item.status)?.color}
+                      background={tagByStatus(item.status)?.background}
+                    />
+                  </Styled.StatusBox>
+                  <section>
+                    <Styled.BrandName>{item.brand}</Styled.BrandName>
+                    <Styled.Starter>
+                      <Image src={tempProfileImg} alt="none" width={16} height={16} />
+                      <span>{item.starter}</span>
+                      <Image src={starterImg} alt="none" />
+                    </Styled.Starter>
+                  </section>
+                </Styled.FundingInfo>
+                <Styled.LimitBox>
+                  <div>
+                    <RiUser3Fill />
                     <div>
-                      <RiUser3Fill />
-                      <div>
-                        <b>
-                          {item.cur_member}/{item.min_member}
-                        </b>
-                        <span> 명 참여</span>
-                      </div>
+                      <b>
+                        {item.curMember}/{item.minMember}
+                      </b>
+                      <span> 명 참여</span>
                     </div>
+                  </div>
+                  <div>
+                    <FaClock />
                     <div>
-                      <FaClock />
-                      <div>
-                        {/*<b>{moment(item.deadline).format('hh:mm')}</b>*/}
-                        <span> 마감</span>
-                      </div>
+                      <b>{moment(item.deadline).format('hh:mm')}</b>
+                      <span> 마감</span>
                     </div>
-                  </Styled.LimitBox>
-                  <Styled.ProgressBox>
-                    <Styled.Percentage percentage={getPercentage(item.cur_price, item.total_price)}>
-                      <span>{`${getPercentage(item.cur_price, item.total_price)}% 달성했어요`}</span>
-                      <span>{`${getKORMoneyString(item.cur_price)}원 / ${getKORMoneyString(item.total_price)}원`}</span>
-                    </Styled.Percentage>
-                    <Styled.ProgressBar percentage={getPercentage(item.cur_price, item.total_price)}>
-                      <div></div>
-                    </Styled.ProgressBar>
-                  </Styled.ProgressBox>
-                </Styled.BrandsCard>
-              </Fragment>
-            )
-        )}
-      </Styled.BrandsBox>
+                  </div>
+                </Styled.LimitBox>
+                <Styled.ProgressBox>
+                  <Styled.Percentage percentage={getPercentage(item.curPrice, item.curPrice * item.curMember)}>
+                    {item.curMember ? (
+                      <span>{`${getPercentage(item.curPrice, item.minPrice)}% 달성했어요`}</span>
+                    ) : (
+                      <span>참여인원이 없습니다.</span>
+                    )}
+                    <span>{`${getKORMoneyString(item.curPrice)}원 / ${getKORMoneyString(item.minPrice)}원`}</span>
+                  </Styled.Percentage>
+                  <Styled.ProgressBar percentage={getPercentage(item.curPrice, item.minPrice)}>
+                    <div></div>
+                  </Styled.ProgressBar>
+                </Styled.ProgressBox>
+              </Styled.BrandsCard>
+            </Fragment>
+          ))}
+        </Styled.BrandsBox>
+      )}
+      {fundingList.length === 0 && !isLoading && (
+        <Styled.EmptySection>
+          <TbNotesOff style={{ width: '24px', height: '24px' }} />
+          empty data
+        </Styled.EmptySection>
+      )}
     </div>
   )
 }
